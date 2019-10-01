5bf3fcc853ad12e33a729a87d7f07ea34740eb09
Samuel Austen <furneauxsam1994@gmail.com>
2019-10-01 13:05:37 +0200

    commit nuxt

---
+++ _nuxt/2eca35a472a453db93f6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{273:function(t,e,n){"use strict";n.r(e);var c={},l=n(44),component=Object(l.a)(c,function(){this.$createElement;this._self._c;return this._m(0)},[function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("h1",[this._v("index")])])}],!1,null,null,null);e.default=component.exports}}]);
